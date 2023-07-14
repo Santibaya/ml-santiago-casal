2d0d70d8396ef816b2310190a01c453288e8d52e
Santibaya <santiagobayacasal@gmail.com>
2023-07-13 22:41:23 -0300

    render

---
+++ Documents/Digital House/proyecto-html/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+const dotenv = require("dotenv").config();
+
+let publicPath = path.join(__dirname, './public')
+app.use(express.static(publicPath))
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
+app.listen(process.env.PORT,() => {
+    console.log('servidor exitoso')
+})
